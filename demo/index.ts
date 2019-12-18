dd98c584945da448fc666af7452432c82ab03084
Marco Solazzi <marco.solazzi@gmail.com>
2019-12-18 15:51:11 +0900

    use the default export

--- demo/index.ts
+++ demo/index.ts
@@ -1,11 +1,11 @@
 import { css } from 'emotion'
-import { scale } from '../src'
+import hms from '../src'
 
 const bodyScale = [0, 3]
 const h1Scale = [3, 0]
 
 document.body.className = css`
-  ${scale.sizes(bodyScale)}
+  ${hms.sizes(bodyScale)}
 `
 
 const h1 = document.createElement('h1')
@@ -15,14 +15,14 @@ h1.textContent = 'harmonic-modular-scale'
 p.textContent = 'Hello, world!'
 
 h1.className = css`
-  ${scale.sizes(h1Scale)}
-  margin-top: ${scale.getLineHeight(bodyScale, 3)};
-  margin-bottom: ${scale.getLineHeight(0, 3, 2)};
+  ${hms.sizes(h1Scale)}
+  margin-top: ${hms.getLineHeight(bodyScale, 3)};
+  margin-bottom: ${hms.getLineHeight(0, 3, 2)};
 `
 
 p.className = css`
-  margin-top: ${scale.getLineHeight(bodyScale)};
-  margin-bottom: ${scale.getLineHeight(bodyScale)};
+  margin-top: ${hms.getLineHeight(bodyScale)};
+  margin-bottom: ${hms.getLineHeight(bodyScale)};
 `
 
 document.body.appendChild(h1)
